29b4ab0cf332ee948246c9170ce1d73173c2315e
t-taiki0620 <harunakunya@gmail.com>
2022-07-22 15:39:42 +0900

    did viewable originData

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import HyperTree from './HyperTree';
-import { useEffect, useState } from 'react';
 
 export default function App() {
-  const [data, setData] = useState(null)
-  useEffect(()=> {
-    (async () => {
-      const dataPath = 'data/mammalia.d3.json'
-      const dataResponse = await fetch(dataPath)
-      const data = await dataResponse.json()
-      setData(data)
-    })()
-  }, [])
-
-  if (data == null) {
-    return <div>loading</div>;
-  }
   return (
     <>
-      <HyperTree data = {data}/>
+      <HyperTree/>
     </>
   );
 }
--- src/HyperTree.js
+++ src/HyperTree.js
@@ -1,14 +1,58 @@
 import * as hyt from 'd3-hypertree';
 import './d3-hypertree-light.css'
+import { useEffect, useState } from 'react';
+import * as d3 from 'd3'
 
-export default function HyperTree({data}){
+export default function HyperTree(){
+    const [data, setData] = useState(null)
+    useEffect(()=> {
+        (async () => {
+            const dataPath = 'data/220719v2.json'
+            const dataResponse = await fetch(dataPath)
+            const data = await dataResponse.json()
+            if (dataPath === 'data/mammalia.d3.json'){
+                setData(data)
+            }
+            else{
+                const stratify = d3
+                .stratify()
+                .id((d) => d.no)
+                .parentId((d) => d.parent)
+                const dataStratify = stratify(data)
+                const root = d3.hierarchy(dataStratify)
+                
+                setData(root)
+            }
+        })()
+    }, [])
+
+    if (data == null) {
+        return <div>loading</div>;
+    }
+
     const ht = new hyt.Hypertree(
         {
             parent: document.body             
         },
         {
             dataloader: hyt.loaders.fromData(data),
-            langInitBFS: (ht, n)=> n.precalc.label = n.data.name,
+            langInitBFS: (ht, n)=> n.precalc.label = n.data.data.data.Title,
+            interaction: {
+                onNodeClick: (n, m, l)=> { 
+                    console.log(`#onNodeClick: Node=${n}, click coordinates=${m}, source layer=${l}`)
+    
+                    ht.api.goto({ re:-n.layout.z.re, im:-n.layout.z.im }, null)
+                        .then(()=> l.view.hypertree.drawDetailFrame())       
+                    /*
+                    var s = n.ancestors().find(e=> true)                
+                    ud.view.hypertree.api.toggleSelection(s)
+                    ud.view.hypertree.args.interaction.onNodeSelect(s)
+                    */
+                    console.log(n.data.data.data.Title)
+                },
+
+                onCenterNodeChange: n=> console.log(`#onCenterNodeChange: Node=${n}`)
+            }
         }
     )
     ht.initPromise
